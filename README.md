d88039a89cda38c21064782e70fff60f15792925
Anton Fagerberg <anton@antonfagerberg.com>
2015-05-18 21:32:42 +0200

    Added modes

--- README.md
+++ README.md
@@ -15,5 +15,10 @@ Written in Elixir.
 1. Get [Elixir](http://elixir-lang.org/)!
 2. Clone this project
 3. `iex -S mix`
-4. `FileRemover.run("/cool_folder", ~r/\.DS_Store/)`
+4. `FileRemover.run("/cool_folder", ~r/\.DS_Store/, :ask)`
 5. Wait and enjoy the show!
+
+You can use the following modes:
+ * `:ask` - Ask if you want to delete or not.
+ * `:print` - Print all matched files.
+ * `:delete` - Delete immediately without asking.
--- lib/file_remover.ex
+++ lib/file_remover.ex
@@ -1,36 +1,69 @@
 defmodule FileRemover do
-  def run(path \\ File.cwd!, pattern \\ ~r/^\.DS_Store$/) do
+  defp delete(path_file) do
+    case File.rm(path_file) do
+      :ok ->
+        puts_ok("Deleted", path_file)
+
+      {:error, reason} ->
+        puts_error "Unable to remove file: #{path_file}, reason: #{reason}"
+    end
+  end
+
+  defp ask(path_file) do
+    case IO.gets("[Y/n] Delete file: #{path_file}? ") do
+      input when is_binary(input) ->
+        cond do
+          input == "Y\n" || input == "y\n" || input == "\n" ->
+            delete(path_file)
+
+          input == "N\n" || input == "n\n" ->
+            :ok
+
+          true ->
+            ask(path_file)
+        end
+
+      _ ->
+        ask(path_file)
+    end
+  end
+
+  def run(path \\ File.cwd!, pattern \\ ~r/^\.DS_Store$/, mode \\ :ask) do
     case File.ls(path) do
       {:ok, file_list} ->
         for file <- file_list do
           if Regex.match?(pattern, file) do
             path_file = "#{path}/#{file}"
-            
-            case File.rm(path_file) do
-              :ok -> 
-                IO.ANSI.format([:black_background, :green, :bright, "[Ok] ", :white, path_file]) 
-                |> IO.puts
-              
-              {:error, reason} ->
-                puts_warning "Unable to remove file: #{path_file}, reason: #{reason}"
+
+            case mode do
+              :delete -> delete(path_file)
+              :ask -> ask(path_file)
+              :print -> puts_ok("Found", path_file)
+              _ -> puts_error("Invalid mode: #{mode}")
             end
           end
         end
-        
+
         for file <- file_list do
           abs_file = "#{path}/#{file}"
-          if File.dir?(abs_file), do: run(abs_file)
+          if File.dir?(abs_file), do: run(abs_file, pattern, mode)
         end
-        
+
         :ok
-      
-      {:error, reason} -> 
-        puts_warning "Unable to list directory: #{path}, reason: #{reason}"
+
+      {:error, reason} ->
+        puts_error "Unable to list directory: #{path}, reason: #{reason}"
+        :ok
     end
   end
-  
-  def puts_warning(message) do
+
+  def puts_error(message) do
     IO.ANSI.format([:black_background, :red, :bright, "[Error] ", :white, message])
+    |> IO.puts
+  end
+
+  def puts_ok(label, message) do
+    IO.ANSI.format([:black_background, :green, :bright, "[#{label}] ", :white, message])
     |> IO.puts
   end
 end
